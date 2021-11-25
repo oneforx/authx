0ec1ce1fe66d22b772ebd269e0c28ce03ad6a92f
Hermann Vincent <baw.developpement@gmail.com>
2021-11-25 10:29:35 +0400

    fix useLocalState error

--- libs/authx-react/src/lib/authx-react.tsx
+++ libs/authx-react/src/lib/authx-react.tsx
@@ -1,14 +1,60 @@
-import './authx-react.module.css';
+import { createContext, useCallback, useEffect, useMemo } from "react";
+import { useLocalState, useRequest } from "@oneforx/poseidon"
+import useTokenState, { TokenState } from "./use-token-state";
+import useConnect from "./use-connect";
 
-/* eslint-disable-next-line */
-export interface AuthxReactProps {}
+const InitialState = {
+  isConnected: false,
+}
 
-export function AuthxReact(props: AuthxReactProps) {
+// const connectWithCred = ( uri: string ) => new Promise((resolve, reject) => {
+//     fetch(uri)
+//     .then((res) => res.json())
+//     .then((data) => data)
+//     .catch((reason) => reject(reason));
+// });
+
+interface IAuthxContextValues {
+    isConnected: boolean,
+    disconnect?: () => void,
+    connectWithCred?: ( email: string, password: string ) => void
+}
+
+export const AuthxContext = createContext<IAuthxContextValues>(InitialState);
+
+interface IAuthxContextProviderProps {
+    url: string,
+    secret_key: string,
+    children: React.ReactChild | React.ReactNode
+}
+
+export const AuthxContextProvider = ({ url, secret_key, children }: IAuthxContextProviderProps) => {
+  const [ authToken, setAuthToken, deleteAuthToken ] = useLocalState<string>("authToken");
+  const authTokenState = useTokenState(authToken && authToken || "");
+  const isConnected = useMemo(() => authTokenState === TokenState.TOKEN_STATE_VALID, [ authTokenState ])
+  const [ connectReponse, connect ] = useConnect(url, { token: authToken && authToken || "" })
+
+  useEffect(() => {
+    if ( !connectReponse.isLoading )
+      if ( !connectReponse.error ) {
+        if ( connectReponse.data !== null && connectReponse.data.msgId === "account_found" ) {
+          setAuthToken( connectReponse.data.msgData.accessToken )
+        }
+      }
+  }, [ connectReponse, setAuthToken ])
+
+  const disconnect = useCallback(() => {
+    deleteAuthToken();
+}, [ deleteAuthToken ]);
+
   return (
-    <div>
-      <h1>Welcome to AuthxReact!</h1>
-    </div>
+      <AuthxContext.Provider value={{
+          isConnected,
+          disconnect,
+          connectWithCred: ( email: string, password: string ) => connect({ body: JSON.stringify({ email, password })}),
+      }}> {children} </AuthxContext.Provider>
   );
 }
 
-export default AuthxReact;
+
+export default AuthxContext
